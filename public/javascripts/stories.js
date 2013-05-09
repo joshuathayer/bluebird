ecf7f8cf082df24f0281bf2ed7af56397b3b87d9
Joshua Thayer <joshuamilesthayer@gmail.com>
2013-05-09 15:29:12 -0400

    Uses foundation CSS and leaflet control layer for button
    Also somewhat improved button & playing state handling.

--- public/javascripts/stories.js
+++ public/javascripts/stories.js
@@ -1,8 +1,7 @@
-
 $(document).ready(function() {
 
     var layer = new L.StamenTileLayer("watercolor");
-    var map = new L.Map("map");
+    var map = new L.Map("map", {zoom: 17, zoomControl: false});
     map.addLayer(layer);
 
     var userMarker;
@@ -11,9 +10,100 @@ $(document).ready(function() {
     var mapInited = false;
     var radius = 20;
     var mySound = null;
-    var playState = false;
 
-    $j("#audio").hide("slow");
+    var playState = false;
+    var buttonState = "hidden";
+
+    // audio state
+    // button state
+    // hidden -- enter region  ->             visible
+    //        <- exit region   --
+    //        <- start playing --  
+    //        -- audio end, but in region -> 
+    // 
+    //           (audio ends)
+    //    v--------------------\
+    // hidden -> available -> playing
+    //    ^---------/
+    //      (move out of region)
+    //
+    // maybe? check for playable content when moving to hidden?
+    // what happens if you're in the location you were just in?
+
+    function buttonToVisibleState() {
+	console.log("toVisibleState, from " + buttonState);
+	console.log("playState is " + playState);
+
+	if ((buttonState != "visible") && !(playState)) {
+	    playControl.addTo(map);
+	    buttonState = "visible";
+	}
+    }
+
+    function buttonToHiddenState() {
+	console.log("toHiddenState, from " + buttonState);
+	console.log("playState is " + playState);
+
+	if (buttonState != "hidden") {
+	    playControl.removeFrom(map);
+	    buttonState = "hidden";
+	}
+	    
+    }
+
+    var PlayControl = L.Control.extend({
+
+	options: {
+		position: 'topleft'
+	},
+
+	onAdd: function (map) {
+
+	    var className = 'row',
+                container = L.DomUtil.create('div', className);
+
+	    this._map = map;
+	    
+	    this._playButton = this._createButton(
+		"a story is available", 'play',  "small round success button",
+		container, this._playStory,  this);
+	    
+            return container;
+	},
+
+	_createButton: function (html, title, className, container, fn, context) {
+		var link = L.DomUtil.create('a', className, container);
+		link.innerHTML = html;
+		link.href = '#';
+		link.title = title;
+
+		var stop = L.DomEvent.stopPropagation;
+
+		L.DomEvent
+		    .on(link, 'click', stop)
+		    .on(link, 'mousedown', stop)
+		    .on(link, 'dblclick', stop)
+		    .on(link, 'click', L.DomEvent.preventDefault)
+		    .on(link, 'click', fn, context);
+
+		return link;
+	},
+
+	_playStory: function () {
+	    mySound.play();
+
+	    mySound.bind("ended", function(e) {
+		playState = false;
+		console.log("playing done. playState is " + playState);
+		buttonToHiddenState();
+	    });
+
+	    playState = true;
+	    buttonToHiddenState();
+	}
+    });
+
+    var playControl = new(PlayControl);
 
     var points = [
 	{ latlng: [40.7255, -73.9877],
@@ -67,28 +157,25 @@ $(document).ready(function() {
 
 	   if (userLocation != null) {
 	       // we were somewhere. now, we're nowhere
-	       $j("#audio").html("");
+	       buttonToHiddenState();
 	   }
 
 	   userLocation = null;
 	   
 	   // we always display our popup, for development
-	   //userMarker.bindPopup("[" + latlng.lat + ", " + latlng.lng + "]");
-	  // userMarker.openPopup();
+	   // userMarker.bindPopup("[" + latlng.lat + ", " + latlng.lng + "]");
+	   // userMarker.openPopup();
        } 
 
        if (inActiveArea.title != userLocation) {
 	   userLocation = inActiveArea.title;
 
 	   // we moved in to a region (either from nothing, or from elsewhere
-	   userMarker.bindPopup("You seem to be in the area of " + inActiveArea.title);
-	   userMarker.openPopup();
+	   //userMarker.bindPopup("You seem to be in the area of " + inActiveArea.title);
+	   //userMarker.openPopup();
 
 	   playNewStory(inActiveArea.story);
-	   
        }
-
-
    };
 
     function playNewStory(storyName) {
@@ -112,13 +199,9 @@ $(document).ready(function() {
 
 	}
 
-	$j("#audio").html("<p>Story available! Touch here to play.");	
-	$j("#audio").show("slow");
 
-	$j("#audio").click(function() {
-	    mySound.play();
-	    $j("#audio").hide("slow");
-	});
+	buttonToVisibleState();
+
     }
 
     
@@ -127,7 +210,7 @@ $(document).ready(function() {
     });
 
     // map.on('click', function(e) {
-    // 	updatedLocation(e.latlng)
+    //  	updatedLocation(e.latlng)
     // });
 
     map.on('locationerror', function() {
@@ -136,7 +219,7 @@ $(document).ready(function() {
 
     map.locate({setView: true,
 		maximumAge: 500,
-		watch: true,
+		watch: false,
 		maxZoom: 18});
 
 })
